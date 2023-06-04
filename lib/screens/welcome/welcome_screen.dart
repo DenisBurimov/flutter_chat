915191852e2d065b17e57421615cb8077e5f26f1
DenisBurimov <denysburimov@gmail.com>
2023-06-04 21:28:32 +0300

    working commit

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -3,6 +3,46 @@ import 'package:flutter/material.dart';
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Image.asset("assets/images/welcome_image.png"),
+            Text(
+              "Simple2chat",
+              textAlign: TextAlign.center,
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineSmall
+                  .copyWith(fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "freedom and safety of communications",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Theme.of(context)
+                      .textTheme
+                      .bodyMedium
+                      .color
+                      .withOpacity(0.64)),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Text("Skip",
+                      style: Theme.of(context).textTheme.bodyLarge.copyWith(
+                          color: Theme.of(context)
+                              .textTheme
+                              .bodyLarge
+                              .color
+                              .withOpacity(0.8)))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
